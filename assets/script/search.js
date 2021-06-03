700ed9a3662d939115ce1d7a52ce2c085cb33468
Caitlin Swickard <caitlin6029@gmail.com>
2021-06-02 20:54:22 -0600

    Update to logo

--- assets/script/search.js
+++ assets/script/search.js
@@ -14,16 +14,19 @@ burgerIcon.addEventListener(`click`, () => {
     navbarMenu.classList.toggle(`is-active`);
 });
 
+
+// function to render lyrics to screen
 function getLyrics() {
+    // API call to fetch lyrics
     let lyricURL = "https://api.lyrics.ovh/v1/" + artist + "/" + song;
+
     fetch(lyricURL)
         .then(function (response) {
             response.json().then(function (data) {
                 console.log(data.lyrics);
-
+                // using html elements to append lyrics
                 const renderLyrics = document.getElementById("render-lyrics")
                 renderLyrics.textContent = data.lyrics
-                renderLyrics.append(lyrics)
             })
         })
 }
@@ -31,25 +34,13 @@ getLyrics();
 
 
 
-
-// function searchWebster() {
-//     let word = $wordInputWebster.val().trim();
-//     let websterURL = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/" + word + "?key=c5d60705-510a-44de-be78-83432ad9714a";
-//     fetch(websterURL)
-//         .then(function (response) {
-//             response.json().then(function (data) {
-//                 $websterDefinition.text(data.shortdef[0]);
-//             })
-//         })
-// }
-
 // function searchUrban() {
 //     let word = $wordInputWebster.val().trim();
 //     let urbanURL = "https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=" + word;
 //     fetch(urbanURL 
 //         "headers": {
-//             "x-rapidapi-key": "1bcc08aea8mshc3a3e71246a7264p1de70cjsnb40efe43f9b3",
-//             "x-rapidapi-host": "mashape-community-urban-dictionary.p.rapidapi.com"
+//         "x-rapidapi-key": "1bcc08aea8mshc3a3e71246a7264p1de70cjsnb40efe43f9b3",
+//         "x-rapidapi-host": "mashape-community-urban-dictionary.p.rapidapi.com"
 
 //     })
 //         .then(function (response) {
@@ -58,3 +49,22 @@ getLyrics();
 //             })
 //         })
 // }
+
+
+
+
+function searchWebster() {
+    let word = $wordInputWebster.val().trim();
+    let websterURL = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/" + word + "?key=c5d60705-510a-44de-be78-83432ad9714a";
+    fetch(websterURL)
+        .then(function (response) {
+            response.json().then(function (data) {
+                console.log(data);
+                $websterDefinition.text(data.shortdef[0]);
+
+
+                // const renderWebster = document.getElementById("render-webster")
+                // renderWebster.textContent = data.
+            })
+        })
+}
