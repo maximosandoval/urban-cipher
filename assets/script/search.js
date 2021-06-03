1c7c0d7115130a92e70f1a9b5d1e081657f3e516
CaitlinSwickard <79483034+CaitlinSwickard@users.noreply.github.com>
2021-06-02 19:51:36 -0600

    Caitlin (#9)
    * Testing JS
    
    * test

--- assets/script/search.js
+++ assets/script/search.js
@@ -1,3 +1,4 @@
+
 let query = window.location.search.substring(1);
 let options = query.split(`&`);
 let songName = options[1].split(`=`);
@@ -16,13 +17,44 @@ burgerIcon.addEventListener(`click`, () => {
 function getLyrics() {
     let lyricURL = "https://api.lyrics.ovh/v1/" + artist + "/" + song;
     fetch(lyricURL)
-        .then (function (response) {
+        .then(function (response) {
             response.json().then(function (data) {
                 console.log(data.lyrics);
+
+                const renderLyrics = document.getElementById("render-lyrics")
+                renderLyrics.textContent = data.lyrics
+                renderLyrics.append(lyrics)
             })
         })
 }
-
-
-
 getLyrics();
+
+
+
+
+// function searchWebster() {
+//     let word = $wordInputWebster.val().trim();
+//     let websterURL = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/" + word + "?key=c5d60705-510a-44de-be78-83432ad9714a";
+//     fetch(websterURL)
+//         .then(function (response) {
+//             response.json().then(function (data) {
+//                 $websterDefinition.text(data.shortdef[0]);
+//             })
+//         })
+// }
+
+// function searchUrban() {
+//     let word = $wordInputWebster.val().trim();
+//     let urbanURL = "https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=" + word;
+//     fetch(urbanURL 
+//         "headers": {
+//             "x-rapidapi-key": "1bcc08aea8mshc3a3e71246a7264p1de70cjsnb40efe43f9b3",
+//             "x-rapidapi-host": "mashape-community-urban-dictionary.p.rapidapi.com"
+
+//     })
+//         .then(function (response) {
+//             response.json().then(function (data) {
+//                 $urbanDefinition.text(data.list[0].definition);
+//             })
+//         })
+// }
