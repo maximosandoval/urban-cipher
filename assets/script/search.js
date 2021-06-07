c0a4c79f34c3d2016e8f0ae755fcacf0fc8ae171
Caitlin Swickard <caitlin6029@gmail.com>
2021-06-06 18:39:50 -0600

    Addition of readme file

--- assets/script/search.js
+++ assets/script/search.js
@@ -30,7 +30,6 @@ function getLyrics() {
     fetch(lyricURL)
         .then(function (response) {
             return response.json()
-
         })
         .then(function (data) {
             console.log(data.lyrics);
@@ -46,7 +45,6 @@ getLyrics();
 // function to search urban dictionary API
 function searchUrban() {
     let word = urbanInput.value;
-
     let urbanURL = "https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=" + word;
     fetch(urbanURL, {
         "headers": {
@@ -74,7 +72,6 @@ urbanButton.addEventListener("click", searchUrban);
 
 // function to search webster dictionary API
 function searchWebster() {
-
     let word = websterInput.value;
     let websterURL = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/" + word + "?key=c5d60705-510a-44de-be78-83432ad9714a";
     fetch(websterURL)
@@ -94,97 +91,6 @@ function searchWebster() {
             }
             renderWebster.append(newUL);
         });
-
-    websterInput.value = '';
-}
-
-        })
-
-        .then(function (data) {
-            console.log(data.lyrics);
-            // attaching lyrics to the html
-            const renderLyrics = document.getElementById("render-lyrics")
-            renderLyrics.textContent = data.lyrics
-        })
-
-}
-getLyrics();
-
-
-// function to search urban dictionary API
-function searchUrban() {
-    let word = urbanInput.value;
-
-    let urbanURL = "https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=" + word;
-    fetch(urbanURL, {
-        "headers": {
-            "x-rapidapi-key": "1bcc08aea8mshc3a3e71246a7264p1de70cjsnb40efe43f9b3",
-            "x-rapidapi-host": "mashape-community-urban-dictionary.p.rapidapi.com"
-        }
-    })
-        .then(function (response) {
-            return response.json()
-        })
-        // creating text content from API call
-        .then(function (response) {
-            let newUL = document.createElement("ul");
-            let deffList = response.list
-            for (var i = 0; i < deffList.length; i++) {
-                let newLi = document.createElement("li")
-                newLi.textContent = deffList[i].definition
-                newUL.append(newLi);
-            }
-            urbanRender.append(newUL);
-        });
-    urbanInput.value = '';
-}
-urbanButton.addEventListener("click", searchUrban);
-
-
-
-// function to search webster dictionary API
-function searchWebster() {
-
-    let word = websterInput.value;
-    let websterURL = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/" + word + "?key=c5d60705-510a-44de-be78-83432ad9714a";
-    fetch(websterURL)
-        .then(function (response) {
-            // creating text content from API call
-
-      return response.json()
-        })
-        .then(function (data) {
-            let newUL = document.createElement("ul");
-            let defList = data;
-            for (var i = 0; i < defList.length; i++) {
-                let newLi = document.createElement("li")
-                newLi.textContent = defList[i].shortdef[0];
-                newUL.append(newLi);
-            }
-            renderWebster.append(newUL);
-        });
-
     websterInput.value = '';
 }
 websterButton.addEventListener("click", searchWebster);
-
-            response.json().then(function (data) {
-                let newUL = document.createElement("ul");
-                let defList = data;
-                for (var i = 0; i < defList.length; i++) {
-                    let newLi = document.createElement("li")
-                    newLi.textContent = defList[i].shortdef[0];
-                    newUL.append(newLi);
-                }
-                renderWebster.append(newUL);
-            });
-        })
-
-    websterInput.value = '';
-}
-
-websterButton.addEventListener('click', searchWebster);
-
-
-
-
